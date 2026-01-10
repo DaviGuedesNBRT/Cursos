1a24b63d10821c5dc765c19ca1b5cf47ef979e39
DaviGuedesNBRT <daviguedesnoberto@outlook.com>
2026-01-10 15:18:56 -0300

    iniciando revisao do curso tecnico

---
+++ python_basico/programacao_estrutural/exercicios/ex_01.py
@@ -0,0 +1,18 @@
+#1. Faça um programa que leia um número inteiro e imprima-o.
+numero = int(input("digite um numero :"))
+print(numero)
+print('')
+
+#2. Faça um programa que peça para o usuário digitar três valores inteiro e imprima a soma deles.
+n1 = int(input("digite um numero :"))
+n2 = int(input("digite um numero :"))
+n3 = int(input("digite um numero :"))
+print(f"a soma dos números digitados são {n1 + n2 + n3}")
+print("")
+
+#3. Faça um programa que recebe três valores e apresente a soma dos quadrados dos valores lidos.
+n4 = int(input("digite um numero :"))
+n5 = int(input("digite um numero :"))
+n6 = int(input("digite um numero :"))
+print(f" os quadrados dos números digitados são {n4*n4}, {n5*n5}, {n6*n6}")
+
---
+++ python_basico/programacao_estrutural/variaveis.py
@@ -0,0 +1,32 @@
+#recebendo dados do usuario atravez do terminal
+
+print("testes recebendo dados atravez do input")
+
+#declarando o tipo da variavel enquanto recebe o valor 
+nome = str(input('informe seu nome :'))
+idade = int(input('informe sua idade :'))
+cpf = int(input('informe seu cpf :'))
+altura = float(input('digite sua altura em metros :'))
+
+#print mais rustico
+print('Olá, {} !! sua idade: {} anos e CPF: {} '.format(nome,idade,cpf))
+
+#teste de outro tipo de print
+print(f"olá {nome}!! seua idade é {idade} anos")
+
+#teste de eoperações no print
+print(f'Olá, {nome}!!! seu ano de nascimento é {2026-idade}')
+
+print(f"olá {nome}!! seua altura é {altura} metros")
+
+#conversão de float para int irá arredondar o numero para baixo!!
+#o int é inpreciso. para melhor precisão, ultiliza-se float
+conversao = int(altura)
+print(conversao)
+
+#é possivel trabalhar com números imaginario ultilizando a tipagem "complex" ou adicionando um "j" ao numero da variavel
+nc = complex(input("escreva um numero para torna-lo imaginario :"))
+nc2 = 5j
+print(f' sao nuemros complexos {nc} e {nc2}')
+
+#não sei ao certo para que iria ultilizar isso, nem na escola entendia a finalidade kkkkk

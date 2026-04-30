6b3662dc6e6a23113388b95256dede09d49fe72d
DaviGuedesNBRT <daviguedesnoberto@outlook.com>
2026-04-30 17:45:50 -0300

    mexendo na parte visual

--- curso_django/projeto1/settings.py
+++ curso_django/projeto1/settings.py
@@ -115,4 +115,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/css/'
+STATIC_URL = '/tatic/'
+STATICFILES_DIRS = [BASE_DIR / "static"] 

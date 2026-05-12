17c0c9353dc9840c66b04ef0e79ebf6380e584c5
DaviGuedesNBRT <daviguedesnoberto@outlook.com>
2026-05-12 13:56:27 -0300

    criando algumas paginas

--- curso_django/receitas/urls.py
+++ curso_django/receitas/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from receitas.views import my_view, register
+from receitas import views
 
 urlpatterns = [
-    path('', my_view, name='home'),
-    path('register/', register, name='register'),
+    path('', views.home, name='home'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('receita/<int:id>/', views.receita, name='receita'),
 ]
--- curso_django/receitas/views.py
+++ curso_django/receitas/views.py
@@ -2,6 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def my_view(request):
+
+def register(request):
+    return render(request, 'receitas/pages/register.html')
+
+def login(request):
+    return render(request, 'receitas/pages/login.html')
+
+def home(request):
     return render(request, 'receitas/pages/index.html')
+
+def receita(request, id):
+    return render(request, 'receitas/pages/receita.html', context={
+        'id': id
+    })
 

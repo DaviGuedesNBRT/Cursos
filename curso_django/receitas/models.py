bf6244f06c755c829d9dc680727b78637db6033a
DaviGuedesNBRT <daviguedesnoberto@outlook.com>
2026-05-13 13:14:15 -0300

    estudando

--- curso_django/receitas/models.py
+++ curso_django/receitas/models.py
@@ -1,3 +1,56 @@
 from django.db import models
 
-# Create your models here.
+
+class TbCategorias(models.Model):
+    cat_id = models.AutoField(primary_key=True)
+    cat_nome = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.cat_nome
+
+
+class TbUsuario(models.Model):
+    usu_id = models.AutoField(primary_key=True)
+    usu_nome = models.CharField(max_length=255)
+    usu_email = models.EmailField(unique=True)
+    usu_senha = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.usu_nome
+
+
+class TbReceitas(models.Model):
+    rec_id = models.AutoField(primary_key=True)
+    rec_nome = models.CharField(max_length=255)
+    rec_descricao = models.TextField()
+    rec_ingredientes = models.TextField()
+    rec_modo_preparo = models.TextField()
+    rec_tempo_preparo = models.IntegerField()
+    rec_rendimento = models.IntegerField()
+
+    rec_categoria = models.ForeignKey(
+        TbCategorias,
+        on_delete=models.SET_NULL, null=True
+    )
+
+    rec_imagem = models.ImageField(
+        upload_to='receitas/receitas_static/imagens/'
+    )
+
+    rec_data_criacao = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    rec_user_id = models.ForeignKey(
+        TbUsuario,
+        on_delete=models.SET_NULL, null=True
+    )
+
+    rec_data_atualizacao = models.DateTimeField(
+        auto_now=True
+    )
+
+    rec_publicada = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.rec_nome

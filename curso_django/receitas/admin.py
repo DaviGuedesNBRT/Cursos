cf311b419107df73ca3b6c1babe1a416a2c4b922
DaviGuedesNBRT <daviguedesnoberto@outlook.com>
2026-05-15 16:41:48 -0300

    trabalho do karai pra usar um base.html

--- curso_django/receitas/admin.py
+++ curso_django/receitas/admin.py
@@ -1,14 +1,11 @@
 from django.contrib import admin
-from .models import tb_categorias, tb_suario, tb_receitas
+from .models import tb_categorias, tb_receitas
 # Register your models here.
 
 @admin.register(tb_categorias)
 class CategoryAdmin(admin.ModelAdmin):
     ...
 
-@admin.register(tb_suario)
-class UsuarioAdmin(admin.ModelAdmin):
-    ...
 
 @admin.register(tb_receitas)
 class ReceitaAdmin(admin.ModelAdmin):

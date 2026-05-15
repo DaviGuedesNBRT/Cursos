a12528f4eaf6fb5f90f35ac7544e157f7cebda9b
DaviGuedesNBRT <daviguedesnoberto@outlook.com>
2026-05-14 22:05:42 -0300

    sei la

--- curso_django/receitas/models.py
+++ curso_django/receitas/models.py
@@ -1,23 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User  # Usar o User do Django
 
 class tb_categorias(models.Model):
     cat_id = models.AutoField(primary_key=True)
     cat_nome = models.CharField(max_length=255)
-
+    
     def __str__(self):
         return self.cat_nome
-
-
-class tb_suario(models.Model):
-    usu_id = models.AutoField(primary_key=True)
-    usu_nome = models.CharField(max_length=255)
-    usu_email = models.EmailField(unique=True)
-    usu_senha = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.usu_nome
-
 
 class tb_receitas(models.Model):
     rec_id = models.AutoField(primary_key=True)
@@ -27,30 +16,16 @@ class tb_receitas(models.Model):
     rec_modo_preparo = models.TextField()
     rec_tempo_preparo = models.IntegerField()
     rec_rendimento = models.IntegerField()
-
-    rec_categoria = models.ForeignKey(
-        tb_categorias,
-        on_delete=models.SET_NULL, null=True
-    )
-
-    rec_imagem = models.ImageField(
-        upload_to='receitas/imagens/'
-    )
-
-    rec_data_criacao = models.DateTimeField(
-        auto_now_add=True
-    )
-
-    rec_user_id = models.ForeignKey(
-        tb_suario,
-        on_delete=models.SET_NULL, null=True
-    )
-
-    rec_data_atualizacao = models.DateTimeField(
-        auto_now=True
-    )
-
+    
+    rec_categoria = models.ForeignKey(tb_categorias, on_delete=models.SET_NULL, null=True)
+    rec_imagem = models.ImageField(upload_to='receitas/imagens/')
+    rec_data_criacao = models.DateTimeField(auto_now_add=True)
+    
+    # Mudar para User do Django
+    rec_user_id = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    
+    rec_data_atualizacao = models.DateTimeField(auto_now=True)
     rec_publicada = models.BooleanField(default=False)
-
+    
     def __str__(self):
         return self.rec_nome
--- curso_django/receitas/views.py
+++ curso_django/receitas/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.urls import reverse
 
 
 def register(request):

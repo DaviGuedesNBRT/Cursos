77c551ca2ade1a820f9426d046ae6db199283c76
DaviGuedesNBRT <daviguedesnoberto@outlook.com>
2026-05-16 17:28:24 -0300

    update

--- curso_django/receitas/views.py
+++ curso_django/receitas/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.urls import reverse
+from .utils.receitas.inject import make_recipe
 
 
 def register(request):
@@ -11,7 +12,9 @@ def login(request):
     return render(request, 'receitas/pages/login.html')
 
 def home(request):
-    return render(request, 'receitas/pages/index.html')
+    return render(request, 'receitas/pages/index.html', context={
+        'receitas': [make_recipe() for _ in range(10)]
+    })
 
 def receita(request, id):
     return render(request, 'receitas/pages/receita.html', context={

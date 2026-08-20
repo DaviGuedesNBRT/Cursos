36a8aaff9aa50790cfc9518571e1409e4407b6b7
DaviGuedesNBRT <daviguedesnoberto@outlook.com>
2026-08-20 10:23:22 -0300

    testando commit antigravity

--- curso_django/receitas/tests/receitas_urls_test.py
+++ curso_django/receitas/tests/receitas_urls_test.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
-from django.urls import reverse, resolve
-import views
+from django.urls import reverse, resolve, client
+
 # Create your tests here.
 
 
@@ -12,3 +12,27 @@ class ReceitaUrlTest(TestCase):
     def teste_receita_detail_correta(self):
         detailUrl = reverse("Receitas:Receitas-detail", kwargs={'id': 1})
         self.assertEqual(detailUrl, '/receita/1/')
+    
+    def teste_receita_nova_correta(self):
+        novaUrl = reverse("Receitas:Receitas-nova-receita")
+        self.assertEqual(novaUrl, '/nova-receita/')
+
+    def teste_receita_minhas_correta(self):
+        minhasUrl = reverse("Receitas:Receitas-minhas-receitas")
+        self.assertEqual(minhasUrl, '/minhas-receitas/')
+
+    def teste_receita_favoritas_correta(self):
+        favoritasUrl = reverse("Receitas:Receitas-receitas-favoritas")
+        self.assertEqual(favoritasUrl, '/receitas-favoritas/')
+
+    def teste_receita_pendentes_correta(self):
+        pendentesUrl = reverse("Receitas:Receitas-receitas-pendentes")
+        self.assertEqual(pendentesUrl, '/receitas-pendentes/')
+
+    def teste_receita_aprovar_correta(self):
+        aprovarUrl = reverse("Receitas:Receitas-aprovar-receita", kwargs={'id': 1})
+        self.assertEqual(aprovarUrl, '/aprovar-receita/1/')
+
+    def teste_receita_home_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
